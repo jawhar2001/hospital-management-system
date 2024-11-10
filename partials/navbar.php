b1cba71b3fa119124725d338adcff37684f6b117
Mohamed Rifkan <59793466+rifkan-py@users.noreply.github.com>
2024-11-10 11:35:22 +0530

    updated

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,4 +1,3 @@
-
 <nav class="navbar">
     <div class="nav-container">
         <div class="logo"><span class="logo-span-1">HopeWell</span> <span class="logo-span-2">Hospital</span></div>
@@ -15,20 +14,27 @@
 
         <?php
         } else {
+            if ($_SESSION['role'] === 'admin') {
         ?>
-            <div class="nav-links">
-                <a href="index.php">Home</a>
-                <a href="contact.php">Contact</a>
-                <a href="about.php">About</a>
-                <a href="all-appointments.php">All Appointment</a>
-                <a href="my-appointments.php">My Appointment</a>
-                <a href="appointments.php">Appointment</a>
-                <a href="doctors.php">Doctors</a>
-                <a href="users.php">Users</a>
-                <a href="signout.php" class="primary-btn">Sign out</a>
-            </div>
-
+                <div class="nav-links">
+                    <a href="index.php">Home</a>
+                    <a href="all-appointments.php">All Appointments</a>
+                    <a href="doctors.php">Doctors</a>
+                    <a href="users.php">Users</a>
+                    <a href="signout.php" class="primary-btn">Sign out</a>
+                </div>
         <?php
+            } else {
+        ?>
+                <div class="nav-links">
+                    <a href="index.php">Home</a>
+                    <a href="contact.php">Contact</a>
+                    <a href="about.php">About</a>
+                    <a href="my-appointments.php">My Appointments</a>
+                    <a href="signout.php" class="primary-btn">Sign out</a>
+                </div>
+        <?php
+            }
         }
         ?>
     </div>
